16374291b087c8da6d51f429f68195046a13e186
Hanric <enricayguade@gmail.com>
2015-03-28 13:41:58 +0100

    Roles added

---
+++ server/boot/role-resolver.js
@@ -0,0 +1,82 @@
+module.exports = function(app) {
+	var Role = app.models.Role;
+
+	//groupMember
+	Role.registerResolver('groupMember', function(role, context, cb) {
+		function reject() {
+			process.nextTick(function() {
+				cb(null, false);
+			});
+		}
+
+		// if the target model is not Group
+		if (context.modelName !== 'Group') {
+			return reject();
+		}
+
+		// do not allow anonymous users
+		var userId = context.accessToken.userId;
+		if (!userId) {
+			return reject();
+		}
+
+		// check if there is a Member with userId and groupId
+		context.model.findById(context.modelId, function(err, group) {
+			if (err || !project)
+				return reject();
+
+			var Member = app.models.Member;
+			Member.count({
+				personId: userId,
+				groupId: group.modelId
+			}, function(err,count) {
+				if (err) {
+					console.log(err);
+					return cb(null, false);
+				}
+
+				cb(null, count > 0); // if count > 0, there is at least a Member that matches
+			});
+		});
+	});
+
+	//groupAdmin
+	Role.registerResolver('groupAdmin', function(role, context, cb) {
+		function reject() {
+			process.nextTick(function() {
+				cb(null, false);
+			});
+		}
+
+		// if the target model is not Group
+		if (context.modelName !== 'Group') {
+			return reject();
+		}
+
+		// do not allow anonymous users
+		var userId = context.accessToken.userId;
+		if (!userId) {
+			return reject();
+		}
+
+		// check if there is a Member with userId and groupId and isAdmin = true
+		context.model.findById(context.modelId, function(err, group) {
+			if (err || !project)
+				return reject();
+
+			var Member = app.models.Member;
+			Member.count({
+				personId: userId,
+				groupId: group.modelId,
+				isAdmin: true
+			}, function(err,count) {
+				if (err) {
+					console.log(err);
+					return cb(null, false);
+				}
+
+				cb(null, count > 0); // if count > 0, there is at least a Member that matches
+			});
+		});
+	});
+}
